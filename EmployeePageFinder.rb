c0e80cb11075b863f975d18bbda50aab6d69826d
Derek <dtiggelaar@cmsdm.com>
2014-01-24 12:00:06 -0600

    Optimized threading - Threads write to separate files now.
    The threads write to separate files and then are combined. I moved the
    actual reading of the pdf into the mutex. Improved run time by 33%

--- EmployeePageFinder.rb
+++ EmployeePageFinder.rb
@@ -1,49 +1,77 @@
 require 'csv'
 require 'pdf-reader'
-require 'debugger'
 
 pagenumber_file = File.join(File.expand_path File.dirname(__FILE__), 'theSubsetPages')
 pdf_filename = File.join(File.expand_path File.dirname(__FILE__), 'subset.pdf')
 @reader = PDF::Reader.new(pdf_filename)
-@writer = CSV.open(pagenumber_file, 'wb')
 @mutex = Mutex.new
 threads = []
 @current_page = 1
 @total_pages = 500 # Length of pdf file
 
+class Page
+	def initialize(number, content)
+		@number = number
+		@content = content
+	end
+
+	def number
+		@number
+	end
+
+	def content
+		@content
+	end
+end
 
 def find_employees
-	while @current_page != @total_pages
-		page = read_page
-		if page.nil? != true
-			page_content = @reader.page(page)
-			if page_content.text.include? "ORG CHARTS AND CONTACTS"
-				if !page_content.text.include? "full org charts" 
-					@writer << [page]
+	valid = CSV.open(File.join(File.expand_path File.dirname(__FILE__), "valid#{Thread.current['id']}.tmp"), 'wb')
+	begin
+		while @current_page < @total_pages
+			page = read_page
+			if page.content.include? "ORG CHARTS AND CONTACTS"
+				if !page.content.include? "full org charts" 
+					valid << [page.number]
 				end
 			end
 		end
+	rescue Exception => err
+		puts err
 	end
+	valid.close
 end
 
 def read_page
 	page = nil
 	@mutex.synchronize do
-		begin
-			page = @current_page
-		rescue
-			puts "Page " + @current_page.to_s + " did not work."
+		if @current_page < @total_pages
+			page_content = @reader.page(@current_page).text
+			page = Page.new(@current_page, page_content)
+			puts @current_page
+			@current_page += 1
 		end
-		puts @current_page
-		@current_page += 1
 	end
 	page
 end
 
-1.upto(20) do |thread|
+def combine_files(pattern, src_file)
+	temp_files = Dir.glob( File.join(File.expand_path File.dirname(__FILE__), pattern) )
+	begin 
+		open(src_file, 'w') do |file|
+			temp_files.each do |f|
+				file.write(IO.read(f))
+			end
+		end
+	rescue Exception => err
+		puts "Error combining temp files - #{err.message}"
+	end
+end
+
+1.upto(10) do |thread|
 	threads << Thread.new { Thread.current['id'] = thread; find_employees }
 end
 threads.each { |t| t.join }
 
-@writer.close
-@reader.close
+combine_files('*.tmp', pagenumber_file)
+
+puts 'done'
